d97f6f2fa7a612424a340854193db8ebcd163987
shreyashivakumar66 <shreyashivakumar66812@gmail.com>
2026-03-23 06:51:49 +0530

    Create MoveZeroes.java

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,19 @@
+//Added move Zeroes solution using two pointer technique
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int insertPos=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != 0){
+
+            int temp=nums[i];
+            nums[i]=nums[insertPos];
+            nums[insertPos]=temp;
+
+            insertPos++;
+        }
+    }
+}
+
+}        
+    
